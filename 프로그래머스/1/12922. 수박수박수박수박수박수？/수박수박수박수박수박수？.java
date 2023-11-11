a283934ebfc76a7072fad41dc5f5fc3c47a4b71d
김규영 <40595896+kky98@users.noreply.github.com>
2023-11-11 17:33:07 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.87 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        for(int i =1 ; i<n+1 ; i++){
+            if(i%2 == 1){
+                answer+="수";
+            }else{
+                answer+="박";
+            }
+        }
+        return answer;
+    }
+}
