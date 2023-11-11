5787428cf95383855e707b5b02d25efb5ad31b55
김규영 <40595896+kky98@users.noreply.github.com>
2023-11-11 17:18:11 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int len = s.length();
+        
+        if(len %2 ==1){
+           answer=s.substring(len/2,len/2+1); 
+        }else{
+            answer=s.substring(len/2-1,len/2+1); 
+            
+        }
+        return answer;
+    }
+}
