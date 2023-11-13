705ec391619fe307e058aea678130939df218eb8
김규영 <40595896+kky98@users.noreply.github.com>
2023-11-13 15:07:29 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.25 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,11 +1,20 @@
 import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
-        int answer =45;
-        for(int num : numbers){
-            answer-=num;
+        int answer =0;
+        HashMap<Integer,Integer> map =new HashMap<>();
+        for(int i=0;i<10;i++){
+            map.put(i,0);
         }
-        
+        for(int num : numbers){
+            map.put(num,1);
+        }
+        System.out.print(map.entrySet());
+        for(Map.Entry<Integer,Integer> en :map.entrySet()){
+            if(en.getValue()==0){
+                answer+=en.getKey();
+            }
+        }
         return answer;
     }
 }
